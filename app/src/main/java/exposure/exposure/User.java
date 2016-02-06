6bf54f1af0a6cc294de2f64591f519b90a6f5f02
Sean Wammer <swammer@cs.washington.edu>
2016-02-06 00:30:14 -0800

    Added User.java I added a skeleton for the User object.

---
+++ app/src/main/java/exposure/exposure/User.java
@@ -0,0 +1,22 @@
+package exposure.exposure;
+
+/**
+ * Represents a user of Exposure.
+ */
+public class User {
+
+    // TODO: decide on what information the User object actually needs
+    private int id;
+    private String username;
+    private String fname;
+    private String lname;
+    private String link;
+    private String aboutMe;
+
+    public User() {
+        // TODO: implement constructor(s)
+
+    }
+
+    // TODO: write getter methods
+}
