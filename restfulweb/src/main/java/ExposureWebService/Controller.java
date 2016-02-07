aa61ef3332529cd57dfc01d8787a8b14630f283d
kekonat <kekonat@gmail.com>
2016-02-07 14:15:56 -0800

    added base objects to manipulate the database. code currently works

--- restfulweb/src/main/java/ExposureWebService/Controller.java
+++ restfulweb/src/main/java/ExposureWebService/Controller.java
@@ -4,29 +4,31 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @RestController
 public class Controller {
 
+	private static DriverManagerDataSource dataSource;
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
     private int i = 15;
+    
+    static {
+    	dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		dataSource.setUrl("jdbc:sqlserver://vyl5xz64ek.database.windows.net;database=Exposure");
+		dataSource.setUsername("kekonat@vyl5xz64ek.database.windows.net");
+		dataSource.setPassword("N0REGRETs");
+    }
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="ControllerGreeting") String name) {
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
     	test();
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
-    }
-    @RequestMapping("/probing")
-    public Probe probing(@RequestParam(value="name", defaultValue="ControllerProbing") String name) {
-        return new Probe(counter.incrementAndGet(),
-                            String.format(template, name));
-    }
-    @RequestMapping("/list")
-    public GreetingList list(@RequestParam(value="name", defaultValue="ControllerList") String name) {
-        return new GreetingList(counter.incrementAndGet(),
-                            String.format(template, name));
+    	JdbcTemplate insert = new JdbcTemplate(dataSource);
+    	insert.update("INSERT INTO PERSON VALUES('testname', 'kekona');");
+        return new Greeting(String.format(template, name));
     }
     
     private void test() {
--- restfulweb/src/main/java/ExposureWebService/Greeting.java
+++ restfulweb/src/main/java/ExposureWebService/Greeting.java
@@ -2,16 +2,11 @@ package ExposureWebService;
 
 public class Greeting {
 
-    private final long id;
     private final String content;
 
     public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
-    }
-
-    public long getId() {
-        return id;
     }
 
     public String getContent() {
