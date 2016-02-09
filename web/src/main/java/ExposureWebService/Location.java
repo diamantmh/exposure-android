edd055958c20872e151e57d600181c2ad5614909
kekonat <kekonat@gmail.com>
2016-02-08 16:49:19 -0800

    copy pasted java classes to web directory for testing purposes

---
+++ web/src/main/java/ExposureWebService/Location.java
@@ -0,0 +1,183 @@
+package ExposureWebService; 
+
+import java.util.*;
+
+/**
+ * Location is an immutable representation of a location on the map.
+ *
+ * @specfield id : long  // uniquely identifies this location for database interactions
+ */
+public class Location {
+    private final long id;
+    private final float lat;
+    private final float lon;
+    private final int totalRating;
+    private final int numOfRatings;
+    private final String name;
+    private final String desc;
+    private Set<Category> categories;
+    // TODO: add comments to Location
+    // private List<Comment> comments;
+
+    /*
+     * class invariant,
+     * name != null
+     * desc != null
+     * categories != null
+     */
+
+    /**
+     * Constructs a Location with the specified parameters.
+     *
+     * Should not be used when inserting a new Location using DatabaseManager.
+     * You should omit the ID in this case. Only use this constructor when you
+     * have an ID provided by DatabaseManager.
+     *
+     * @param id - unique identifier supplied by DatabaseManager
+     * @param lat - latitude of this location
+     * @param lon - longitude of this location
+     * @param totalRating - the total number of points this location has earned
+     * @param numOfRatings - the total number of reviews this location has received
+     * @param name - name of location
+     * @param desc - description of this location
+     * @param categories - all the tags associated with this location
+     */
+    public Location(long id, float lat, float lon, int totalRating,
+                    int numOfRatings, String name, String desc,
+                    Set<Category> categories) {
+        this.id = id;
+        this.lat = lat;
+        this.lon = lon;
+        this.totalRating = totalRating;
+        this.numOfRatings = numOfRatings;
+        this.name = name;
+        this.desc = desc;
+        // safe deep copy in
+        categories = new HashSet<>();
+        for (Category cat: categories) {
+            this.categories.add(cat);
+        }
+    }
+
+    /**
+     * Constructs a Location with the specified parameters.
+     *
+     * The ID parameter is omitted. This constructor should be used when using
+     * DatabaseManager to insert a new Location into the database.
+     *
+     * @param lat - latitude of this location
+     * @param lon - longitude of this location
+     * @param totalRating - the total number of points this location has earned
+     * @param numOfRatings - the total number of reviews this location has received
+     * @param name - name of location
+     * @param desc - description of this location
+     * @param categories - all the tags associated with this location
+     */
+    public Location(float lat, float lon, int totalRating,
+                     int numOfRatings, String name, String desc,
+                     Set<Category> categories) {
+        this(-1, lat, lon, totalRating, numOfRatings, name, desc, categories);
+    }
+
+    /**
+     * Returns the unique identifier for this Location.
+     *
+     * The returned ID can be used to interact with DatabaseManager.
+     *
+     * @return the unique identifier of this user or -1 if this Location has
+     * no ID (if ID omitted when constructed)
+     */
+    public long getID() {
+        return id;
+    }
+
+    /**
+     * Returns the latitude of this Location.
+     *
+     * @return the latitude of this Location
+     */
+    public float getLat() {
+        return lat;
+    }
+
+    /**
+     * Returns the longitude of this Location.
+     *
+     * @return the longitude of this Location
+     */
+    public float getLon() {
+        return lon;
+    }
+
+    /**
+     * Returns the total number of points this Location has earned
+     *
+     * @return the total number of points this Location has earned
+     */
+    public int getTotalRating() {
+        return totalRating;
+    }
+
+    /**
+     * Returns the total number of reviews this Location has received.
+     *
+     * @return the total number of reviews this location has received
+     */
+    public int getNumOfRatings() {
+        return numOfRatings;
+    }
+
+    /**
+     * Returns the average rating this Location has received.
+     *
+     * @return the average rating this location has received
+     */
+    public double getRating() {
+        return (double) (totalRating) / numOfRatings;
+    }
+
+    /**
+     * Returns a new Location with the updated rating.
+     *
+     * @param newRating - the new review to add to this Location
+     * @return a new Location with the updated rating
+     */
+    public Location addRating(int newRating) {
+        return new Location(id, lat, lon, totalRating + newRating,
+                numOfRatings + 1, name, desc, getCategories());
+    }
+
+    /**
+     * Returns the name of this Location.
+     *
+     * @return the name of this Location
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Return the description of this Location
+     *
+     * @return the description of this Location
+     */
+    public String getDesc() {
+        return desc;
+    }
+
+    /**
+     * Returns a set of all the categories, or tags, associated with this
+     * Location.
+     *
+     * @return a set of all categories, or tags, associated with this
+     * Location
+     */
+    public Set<Category> getCategories () {
+        Set<Category> retSet = new HashSet<>();
+        // safe deep copy out
+        for (Category cat: categories) {
+            retSet.add(cat);
+        }
+        return retSet;
+    }
+}
---
+++ web/src/main/java/ExposureWebService/Photo.java
@@ -0,0 +1,129 @@
+package ExposureWebService; 
+
+import java.util.Date;
+import java.sql.Time;
+
+/**
+ * Photo is an immutable representation of a photo.
+ *
+ * @specfield id : long  // uniquely identifies this photo for database interactions
+ */
+public final class Photo {
+    private final long id;
+    private final long authorID;
+    private final long locID;
+    private final String source;
+    private final Date date;
+    private final Time time;
+
+    /*
+     * class invariant,
+     * source != null
+     * date != null
+     * time != null
+     */
+
+    /**
+     * Constructs a Photo with the specified parameters.
+     *
+     * Should not be used when inserting a new Photo using DatabaseManager. You
+     * should omit the ID in this case. Only use this constructor when you have
+     * an ID provided by DatabaseManager.
+     *
+     * @param id - unique identifier supplied by DatabaseManager
+     * @param authorID - unique identifier of the author of photo, supplied by DatabaseManager
+     * @param locID - unique identifier of location where photo was taken,
+     *              supplied by DatabaseManager
+     * @param source - source link of photo
+     * @param date - date photo was taken
+     * @param time - time photo was taken
+     */
+    public Photo(long id, long authorID, long locID, String source, Date date, Time time) {
+        this.id = id;
+        this.authorID = authorID;
+        this.locID = locID;
+        this.source = source;
+        this.date = new Date(date.getTime());
+        this.time = new Time(time.getTime());
+    }
+
+    /**
+     * Constructs a Photo with the specified parameters.
+     *
+     * The ID parameter is omitted. This constructor should be used when using
+     * DatabaseManager to insert a new photo into the database.
+     *
+     * @param authorID - unique identifier of the author of photo, supplied by DatabaseManager
+     * @param locID - unique identifier of location where photo was taken,
+     *              supplied by DatabaseManager
+     * @param source - source link of photo
+     * @param date - date photo was taken
+     * @param time - time photo was taken
+     */
+    public Photo(long authorID, long locID, String source, Date date, Time time) {
+        this(-1, authorID, locID, source, date, time);
+    }
+
+    /**
+     * Returns the unique identifier for this photo.
+     *
+     * The returned ID can be used to interact with DatabaseManager.
+     *
+     * @return the unique identifier of this photo or -1 if this Photo has
+     * no ID (if ID omitted when constructed)
+     */
+    public long getID() {
+        return id;
+    }
+
+    /**
+     * Returns the unique identifier of the User that originally posted this
+     * photo.
+     *
+     * The returned ID can be used to interact with DatabaseManager.
+     *
+     * @return the unique identifier of the User that posted this photo
+     */
+    public long getAuthorID() {
+        return authorID;
+    }
+
+    /**
+     * Returns the unique identifier of the location where this photo was
+     * taken.
+     *
+     * The returned ID can be used to interact with DatabaseManager.
+     *
+     * @return the unique identifier of this user
+     */
+    public long getLocID() {
+        return locID;
+    }
+
+    /**
+     * Returns the source link to the picture as a String
+     *
+     * @return the source link to the profile picture of this user
+     */
+    public String getSource() {
+        return source;
+    }
+
+    /**
+     * Returns the Date this photo was taken.
+     *
+     * @return the Date this photo was taken.
+     */
+    public Date getDate() {
+        return new Date(date.getTime());
+    }
+
+    /**
+     * Returns the Time this photo was taken.
+     *
+     * @return the Time this photo was taken.
+     */
+    public Time getTime() {
+        return new Time(time.getTime());
+    }
+}
