4e9176969d1ab14db294dc2c001a4528c5d133a9
swammer5 <swammer@cs.washington.edu>
2016-02-13 17:13:51 -0800

    Added DataDriver This is a stand-alone main file used to test out DatabaseManager and the web service integration.

---
+++ app/src/main/java/exposure/exposure/DataDriver.java
@@ -0,0 +1,20 @@
+package exposure.exposure;
+
+/**
+ * Created by swammer on 2/13/2016.
+ */
+public class DataDriver {
+    public static void main(String[] args ) {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("-----------------");
+        System.out.println("I'm running now!!");
+        System.out.println("-----------------");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
