71c8065efb5e895484994ef388f486b7f69165ad
Calob Symonds <symondsc@cs.washington.edu>
2016-02-16 18:22:31 -0800

    Fixed a bug where the Facebook SDK wasn't properly initialized and the app would crash, and added in the buttons into the Map, List, and ProfileView Activities (no images at the moment: can be discussed later)

--- app/src/main/java/io.github/getExposure/MapsActivity.java
+++ app/src/main/java/io.github/getExposure/MapsActivity.java
@@ -1,8 +1,13 @@
 package io.github.getExposure;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
+import com.facebook.FacebookSdk;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,6 +16,8 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    ImageButton toListView;
+    ImageButton toProfileView;
 
     private GoogleMap mMap;
 
@@ -18,10 +25,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        toListView = (ImageButton) findViewById(R.id.toListView);
+
+        toListView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent listViewIntent = new Intent(getApplicationContext(), ListActivity.class);
+                startActivity(listViewIntent);
+            }
+        });
+
+        toProfileView = (ImageButton) findViewById(R.id.toProfileView);
+
+        toProfileView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent profileViewIntent = new Intent(getApplicationContext(), ProfileViewActivity.class);
+                startActivity(profileViewIntent);
+            }
+        });
     }
 
 
--- app/src/main/java/io.github/getExposure/ProfileViewActivity.java
+++ app/src/main/java/io.github/getExposure/ProfileViewActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.facebook.FacebookSdk;
 import com.facebook.Profile;
 import com.facebook.login.widget.ProfilePictureView;
 
@@ -15,12 +17,22 @@ public class ProfileViewActivity extends AppCompatActivity {
     private ProfilePictureView picview;
     private TextView profilename;
     private Button loginViewSwitcher;
+    ImageButton toMapView;
+    ImageButton toListView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_view);
 
         profile = Profile.getCurrentProfile();
+
+        if (profile == null) {
+            Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(loginIntent);
+            return;
+        }
 
         profilename = (TextView) findViewById(R.id.profilename);
         profilename.setText(profile.getName());
@@ -34,6 +46,24 @@ public class ProfileViewActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent loginViewIntent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(loginViewIntent);
+            }
+        });
+
+        toMapView = (ImageButton) findViewById(R.id.toMapView);
+
+        toMapView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent mapViewIntent = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(mapViewIntent);
+            }
+        });
+
+        toListView = (ImageButton) findViewById(R.id.toListView);
+
+        toListView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent listViewIntent = new Intent(getApplicationContext(), ListActivity.class);
+                startActivity(listViewIntent);
             }
         });
     }
