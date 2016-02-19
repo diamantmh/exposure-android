9f398f5433f5b5823118f7995ac4e9b381b72aac
swammer5 <swammer@cs.washington.edu>
2016-02-19 01:56:11 -0800

    Added two new tests to DataDriver for getting all user and location photos.

--- app/src/main/java/exposure/exposure/DataDriver.java
+++ app/src/main/java/exposure/exposure/DataDriver.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v4.app.FragmentActivity;
 
+import java.io.File;
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Date;
@@ -41,72 +42,43 @@ public class DataDriver extends FragmentActivity {
 
         System.out.println("Trying out some functionality...");
 
-/*
-        // test bad insert user
-        System.out.println();
-        System.out.println("Inserting bogus user,");
-        User bogusUser = new User(50, "I'm bogus", "link", "There's nothing to say about me");
-        long resID = man.insert(bogusUser);
-        System.out.println("Returned ID is: " + resID);
-        if (resID != -1) { throw new AssertionError(); }
-*/
-        // test good insert user
+        // test insert user
         System.out.println();
         System.out.println("Inserting a valid user,");
-        User newUser = new User("good user", "link", "I'm a good user, I hope I make it to the database!");
+        User newUser = new User("Hope", "pray", "believe that this will work");
         displayUser(newUser);
         long userID = man.insert(newUser);
+        System.out.println("ID = " + userID);
         if (userID <= 0) { throw new AssertionError(); }
         System.out.println("User has been inserted");
         User retUser = newUser.addID(userID);
         displayUser(retUser);
-/*
-        // test bad update user
-        System.out.println();
-        System.out.println("Updating new user with no ID (bad operation)");
-        boolean badRes = man.update(new User(newUser.getUsername(), newUser.getLink(), "This won't be saved!"));
-        if (badRes) { throw new AssertionError(); }
-        System.out.println("User was not updated");
 
-        // test good update user
+        // test update user
         System.out.println();
-        System.out.println("Updating user that was just inserted with ID = " + userID);
-        boolean goodRes = man.update(new User(retUser.getID(), retUser.getUsername(), retUser.getLink(), "The about me section is different now!"));
+        System.out.println("Updating user that was just inserted with ID = " + 10);
+        boolean goodRes = man.update(new User(10, "updated username", "updated link", "The about me section is different now!"));
         if (!goodRes) { throw new AssertionError(); }
         System.out.println("User updated");
 
-        // test bad get user
-        System.out.println();
-        System.out.println("Attempting to retrieve a user that doesn't exist");
-        User noResultUser = man.getUser(-1);
-        if (noResultUser != null) { throw new AssertionError(); }
-        System.out.println("Returned null because no user with this ID");
-*/
-
-        // test good get user
+        // test get user
         System.out.println();
         System.out.println("Retrieving updated user with ID = " + 3);
         User retrievedUser = man.getUser(3);
         if (retrievedUser == null) { throw new AssertionError(); }
         System.out.println("Updated user:");
         displayUser(retrievedUser);
-/*
-        // test bad remove user
-        System.out.println();
-        System.out.println("Attempting to remove a user that doesn't exist (id = -1)");
-        boolean remResBad = man.removeUser(-1);
-        if (remResBad) { throw new AssertionError(); }
-        System.out.println("User not removed because user does not exist.");
 
-        // test good remove user
+        // test remove user
         System.out.println();
-        System.out.println("Removing user with id = " + userID);
-        boolean remResGood = man.removeUser(userID);
+        System.out.println("Removing user with id = " + 4);
+        boolean remResGood = man.removeUser(4);
         if (!remResGood) { throw new AssertionError(); }
         System.out.println("User removed");
 
+
         // location tests
-        // test good insert location
+        // test insert location
         System.out.println();
         System.out.println("Inserting new location with no ID specified");
         Category cat = new Category(Category.WALKING_ID);
@@ -115,11 +87,11 @@ public class DataDriver extends FragmentActivity {
         Location newLoc = new Location(10,10,20,4,"Quad", "There are nice trees here!", cats, new ArrayList<Comment>());
         displayLocation(newLoc);
         long retLocID = man.insert(newLoc);
+        System.out.println("Returned ID is: " + retLocID);
         if (retLocID <= 0) { throw new AssertionError(); }
         Location retLoc = newLoc.addID(retLocID);
-        System.out.println("Returned ID is: " + retLocID);
 
-        // test good insert comment
+        // test insert comment
         System.out.println();
         System.out.println("Inserting new comment with no ID specified");
         Comment newCom = new Comment(retUser.getID(), retLoc.getID(), "This is a comment.", new Date(100000), new Time(100000));
@@ -128,7 +100,7 @@ public class DataDriver extends FragmentActivity {
         Comment retCom = newCom.addID(comID);
         System.out.println("Returned ID is: " + comID);
 
-        // test good update location
+        // test update location
         System.out.println();
         System.out.println("Updating location that was just inserted with ID = " + retLocID);
         List<Comment> comments = new ArrayList<>();
@@ -139,31 +111,51 @@ public class DataDriver extends FragmentActivity {
         if (!goodLocRes) { throw new AssertionError(); }
         System.out.println("Location updated with a comment");
 
-        // test good get location
+        // test get location
         System.out.println();
         System.out.println("Retrieving updated location with ID = " + retLocID);
         Location retrievedLocation = man.getLocation(retLocID);
         if (retrievedLocation == null) { throw new AssertionError(); }
         System.out.println("Updated location:");
         displayLocation(retrievedLocation);
-*/
-/*
-        // test good insert photo
+
+        // test insert photo
         System.out.println();
         System.out.println("Inserting new photo with no ID specified");
-        Photo newPhoto = new Photo(retUser.getID(),updatedLocation.getID(),"sourcelink",new Date(100000),new Time(100000));
+        System.out.println("Creating a photo...");
+        File newPNG = new File("exposure-logo.png");
+        Photo newPhoto = new Photo(1,1,"",new Date(0),new Time(0), newPNG);
         long photoID = man.insert(newPhoto);
         if (photoID <= 0) { throw new AssertionError(); }
         Photo retPhoto = newPhoto.addID(photoID);
         System.out.println("Returned ID is: " + photoID);
 
-        // test good remove photo
+        // test remove photo
         System.out.println();
         System.out.println("Removing user with id = " + retPhoto.getID());
         boolean removePhotoRes = man.removePhoto(retPhoto.getID());
         if (removePhotoRes) { throw new AssertionError(); }
         System.out.println("Photo removed");
-*/
+
+        // test get user photos
+        System.out.println();
+        System.out.println("Retrieving user photos from user with id = " + 1);
+        Photo[] userPhotos = man.getUserPhotos(1);
+        if (userPhotos == null) {
+            System.out.println("There are no returned photos! The return array is null.");
+        } else {
+            System.out.println("There were " + userPhotos.length + " photos returned.");
+        }
+
+        // test get location photos
+        System.out.println();
+        System.out.println("Retrieving location photos from location with id = " + 1);
+        Photo[] locPhotos = man.getLocationPhotos(1);
+        if (locPhotos == null) {
+            System.out.println("There are no returned photos! The return array is null.");
+        } else {
+            System.out.println("There were " + locPhotos.length + " photos returned.");
+        }
 
         System.out.println("Thanks for using the DatabaseManager Demo!");
         System.exit(0);
